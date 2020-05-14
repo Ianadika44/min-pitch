f43088727978acec79895d9d650f537c18c68e91
Ianadika44 <adika19ian@gmail.com>
2020-05-15 01:08:50 +0300

    added the login validation

--- pitch/__init__.py
+++ pitch/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 
 
 app = Flask(__name__)
@@ -8,5 +10,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f7266d21c86028741a1b8f2c9ce6d43f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from pitch import routes
